fcf063e38ba91527108097567f984567a100bb92
Anton Vlasenko <anton.vlasenko94@gmail.com>
2022-08-17 14:30:18 +0100

    Generate random age

--- character_creation_randomiser.js
+++ character_creation_randomiser.js
@@ -1,4 +1,4 @@
-// Returns array of UTF-16 code units for 26 capital letters of the alphabet
+// Stores array of UTF-16 code units for 26 capital letters of the alphabet
 const utfString = Array.from(Array(26)).map((e, i) => i + 65);
 const alphabet = utfString.map((x) => String.fromCharCode(x));
 
@@ -6,6 +6,8 @@ const alphabet = utfString.map((x) => String.fromCharCode(x));
 const capitalLetter1 = alphabet[Math.floor(Math.random()*alphabet.length)];
 const capitalLetter2 = alphabet[Math.floor(Math.random()*alphabet.length)];
 
+// Returns random age between 18 and 100
+const age = Math.floor(Math.random() * 83) + 18;
 
 
 /* Code checks:
@@ -13,4 +15,5 @@ console.log(alphabet);
 console.log(utfString);
 console.log(capitalLetter1);
 console.log(capitalLetter2);
+console.log(age);
 */
